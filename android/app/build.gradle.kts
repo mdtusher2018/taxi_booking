a20abe7aa46b9fabad006897ec0e5d5d9de45a1f
mdtusher2025 <mdtusher.bdcalling@gmail.com>
2026-03-31 11:15:33 +0600

    workflow update-3

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -62,14 +62,12 @@ android {
         }
     }
 signingConfigs {
-     create("release") {
-            if(keystorePropertiesFile.exists()){
-                keyAlias = keystoreProperties["keyAlias"] as String
-                keyPassword = keystoreProperties["keyPassword"] as String
-                storeFile = file(keystoreProperties["storeFile"] as String)
-                storePassword = keystoreProperties["storePassword"] as String
-            }
-        }
+    create("release") { 
+        storeFile = file(keystoreProperties["storeFile"] as String)
+        storePassword = keystoreProperties["storePassword"] as String 
+        keyAlias = keystoreProperties["keyAlias"] as String 
+        keyPassword = keystoreProperties["keyPassword"] as String 
+    } 
 }
 
     buildTypes {
